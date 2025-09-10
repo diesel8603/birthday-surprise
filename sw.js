5d59b9474bf3161ed82f6403d984a9711958fcdb
diesel8603 <dieselllllllll89@gmail.com>
2025-09-10 23:31:10 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,29 +3,27 @@ const urlsToCache = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icon-192.png",
+  "./icon-193.png",
   "./icon-512.png"
 ];
 
-// Install
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate (eski cache temizleme)
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
 });
 
-// Fetch (önce cache, sonra internet)
 self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
+
