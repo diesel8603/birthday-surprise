0eaa330b765a6c6db16a20f2ada4f124f077a765
diesel8603 <dieselllllllll89@gmail.com>
2025-09-10 17:43:31 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,31 @@
+const CACHE_NAME = "birthday-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+// Install
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("v1").then(cache => {
-      return cache.addAll([
-        "/",          // ana sayfa
-        "index.html",
-        "style.css",
-        "script.js",
-        "icon-192.png",
-        "icon-512.png"
-      ]);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activate (eski cache temizleme)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+// Fetch (önce cache, sonra internet)
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
